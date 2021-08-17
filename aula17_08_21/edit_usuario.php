6f9397ff654bb29777b3f01f57edd21b876c55eb
Erik Yen <64604368+Erikyen@users.noreply.github.com>
2021-08-17 16:51:50 -0300

    Aula do dia 17/08/21

---
+++ aula17_08_21/edit_usuario.php
@@ -0,0 +1,38 @@
+﻿<?php
+session_start();
+include_once("conexao.php");
+$result_usuario ="SELECT * FROM usuarios WHERE id = '2'";
+$resultado_usuario = mysqli_query($conn, $result_usuario); 
+$row_usuario = mysqli_fetch_assoc($resultado_usuario);
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>CRUD - Cadastrar</title>		
+	</head>
+	<body>
+		<a href="index.php">Editar</a><br>
+		<a href="listar.php">Listar</a><br>
+		<h1>Editar Usuário</h1>
+		<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+		?>
+		<form method="POST" action="proc_edit_usuario.php">
+			<input type="hidden" name="id" placeholder="Digite o seu melhor email" value="<?php 
+			echo $row_usuario['email']; ?>">
+			<label>Nome: </label>
+			<input type="text" name="nome" placeholder="Digite o nome completo" value="<?php echo
+			$row_usuario['nome']; ?>"><br><br>
+			
+			<label>E-mail: </label>
+			<input type="email" name="email" placeholder="Digite o seu melhor e-mail" value="<?php echo
+			$row_usuario['email']; ?>"><br><br>
+			
+			<input type="submit" value="Editar">
+		</form>
+	</body>
+</html>
